2cb183fa324545be640804c0e6a3ece8387903da
xiaoyuzheng <xiaoyuzheng@huatongsilver.com>
2019-03-18 16:28:07 +0800

    更新

--- src/android/JPushTunnelPlugin.java
+++ src/android/JPushTunnelPlugin.java
@@ -8,6 +8,9 @@ import android.text.TextUtils;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
 /**
  * Created by mac005 on 2018/6/5.
  */
